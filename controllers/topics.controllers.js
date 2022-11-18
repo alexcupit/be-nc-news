dd348e48a1e8eb5974d0dfff392cf83f76e8010a
alexcupit <alexcupit@outlook.com>
2022-11-18 11:56:52 +0000

    22 adds post topic

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,7 +1,15 @@
-const { fetchTopics } = require('../models/topics.models');
+const { fetchTopics, insertTopic } = require('../models/topics.models');
 
 exports.getTopics = (req, res) => {
   fetchTopics().then((topics) => {
     res.status(200).send({ topics });
   });
+};
+
+exports.postTopic = (req, res, next) => {
+  insertTopic(req.body)
+    .then((topic) => {
+      res.status(200).send({ topic });
+    })
+    .catch(next);
 };
